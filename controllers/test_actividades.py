5afff073d8a22ac6bd82004007cb0a45cd019702
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-09-17 21:33:31 -0500

    chore: agregue endpoint para creacion de actividades

--- controllers/test_actividades.py
+++ controllers/test_actividades.py
@@ -25,3 +25,16 @@ class TestActividadesController(TestCase):
             }]))
 
         self.assertEqual(respuesta.status_code, 201)
+
+    def test_fail_post_actividades(self):
+        respuesta = self.app.post('/actividades')
+
+        self.assertEqual(respuesta.status_code, 400)
+        self.assertDictEqual(respuesta.json, dict(
+            message={'actividadNombre': 'Falta la actividadNombre'}))
+
+    def test_post_actividades(self):
+        respuesta = self.app.post('/actividades', json={'actividadNombre': 'Hacer la tarea de backend'})
+
+        self.assertEqual(respuesta.status_code, 201)
+        self.assertIsNotNone(respuesta.json)
